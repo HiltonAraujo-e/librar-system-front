2cb7547a0b73bd2014a6021667d771095fc89b6d
Hilton Araujo <hiltoneduardoaraujo@gmail.com>
2025-04-11 22:54:36 +0200

    feat: adicionar endpoint GET_ONLINE_USER para obter informações do usuário autenticado

--- data/client/endpoints.ts
+++ data/client/endpoints.ts
@@ -5,6 +5,7 @@ export const API_ENDPOINTS = {
     me: "/auth/me",
     allBooks: "/books",
     GET_BOOKS: "/book",
+    GET_ONLINE_USER: "/me",
 
     // GET PARAMETERS
     GET_BOOK_DETAILS: (id: string) => `/book/${id}`,
